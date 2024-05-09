e46a6c336d79393c4843f3cf1509f1ff9fbebbc0
Karthi Teja Malyala <kmalyala@microsoft.com>
2024-05-09 09:16:51 +0530

    initial

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "asp_id" {
+  description = "ID of project VPC"
+  value       = azurerm_service_plan.example.id
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable resourceGroupName {
+  type        = string
+  default     = "tf-example"
+  description = "Sample resource group name"
+}
+
+
+variable aspName {
+  type        = string
+  default     = "tf-asp"
+  description = "App Service Plan Name"
+}
+
+
+variable webappName {
+  type        = string
+  default     = "kmalyala-tf-webapp"
+  description = "sample webapp name"
+}
